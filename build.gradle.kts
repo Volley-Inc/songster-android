5a1557a906063be21d66f8fd46ed4d1043eb4460
Sam Wolfand <sam.wolfand@gmail.com>
2024-10-22 13:01:37 -0700

    add songs.json to assets

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,11 +1,26 @@
+import com.android.build.api.dsl.CommonExtension
+import com.android.build.api.dsl.LibraryExtension
+
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
-    alias(libs.plugins.android.application) apply false
-    alias(libs.plugins.kotlin.android) apply false
-    alias(libs.plugins.kotlin.compose) apply false
-    alias(libs.plugins.android.library) apply false
+  alias(libs.plugins.android.application) apply false
+  alias(libs.plugins.kotlin.android) apply false
+  alias(libs.plugins.kotlin.compose) apply false
+  alias(libs.plugins.android.library) apply false
 }
 
 subprojects {
-    plu
+  val commonAndroidConfig: CommonExtension<*, *, *, *, *, *>.() -> Unit = {
+    compileSdk = 34
+    compileOptions {
+      sourceCompatibility = libs.versions.jvmTarget.map(JavaVersion::toVersion).get()
+      targetCompatibility = libs.versions.jvmTarget.map(JavaVersion::toVersion).get()
+    }
+  }
+  pluginManager.withPlugin("com.android.library") {
+    with(extensions.getByType<LibraryExtension>()) {
+      commonAndroidConfig()
+      defaultConfig { minSdk = 31 }
+    }
+  }
 }
